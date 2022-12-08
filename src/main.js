135d307ce8d70a621de6ab740d5b598c07300466
Chung-Chi-Lin <z0925955648@gmail.com>
2022-12-08 22:54:23 +0800

    first update

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+import App from './App.vue';
+import router from './router';
+import { currency, date } from './methods/filters';
+import $httpMessageState from './methods/pushMessageState';
+
+const app = createApp(App);
+app.config.globalProperties.$filters = {
+  currency, date,
+};
+app.config.globalProperties.$httpMessageState = $httpMessageState;
+app.use(VueAxios, axios);
+app.use(router);
+app.component('loading-page', Loading);
+app.mount('#app');
---
+++ src/views/DashBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <Navbar></Navbar>
+  <div class="container-fluid mt-3 position-relative">
+    <ToastMessages></ToastMessages>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import emitter from '@/methods/mitt';
+import ToastMessages from '@/components/ToastMessages.vue';
+import Navbar from '../components/NavbarComponent.vue';
+
+export default {
+  components: {
+    Navbar,
+    ToastMessages,
+  },
+  created() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    this.$http.defaults.headers.common.Authorization = token;
+    const api = `${process.env.VUE_APP_API}api/user/check`;
+    this.$http.post(api, this.user).then((res) => {
+      if (!res.data.success) {
+        this.$router.push('/login');
+      }
+    });
+  },
+  provide() {
+    return {
+      emitter,
+    };
+  },
+};
+</script>
